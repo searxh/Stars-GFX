6b0122df8813388454e3398f0405af7c119f78a6
search <search35453@gmail.com>
2022-12-07 19:48:41 +0700

    improved tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontSize: {
+      '2xs': '.625rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    extend: {
+      fontFamily:{
+        'quicksand':'Quicksand',
+        'righteous':'Righteous'
+      },
+      animation:{
+        'pulse-slow': 'pulse 5s ease-in-out infinite',
+        'spin-slow': 'spin 50s infinite',
+        'pulse-ultra-slow': 'pulse 20s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
