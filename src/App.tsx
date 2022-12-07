564d7a15ba24b87450c6ddc1b9caa65dbdcbb7e7
search <search35453@gmail.com>
2022-12-07 22:06:58 +0700

    new route set up for orders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Contacts from "./pages/Contacts";
+import Orders from "./pages/Orders";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Contacts />} />
+			<Route path="/orders" element={<Orders />} />
 		</Routes>
 	);
 }
--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contacts = () => {
-	return <div className="bg-black text-white">Contacts</div>;
+	return <div className="bg-black text-white font-quicksand">Contacts</div>;
 };
 
 export default Contacts;
