72afb576e9220456c6d2239933dd44ff3f65ecc1
search <search35453@gmail.com>
2022-12-10 12:27:35 +0700

    tested post,put,delete,create

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import axios from "axios";
 import OrderItem from "../components/OrderItem";
+import { v4 as uuidv4 } from "uuid";
 
 const Orders = () => {
     const [orders, setOrders] = React.useState<any>([]);
-    const handleOnClick = () => {};
+    const handleOnClick = () => {
+        axios.post(process.env.REACT_APP_API_URL + "/v1.1/orderlists/", {
+            title: "Beyond Set",
+            status: "Waiting for review",
+            discord: "Nice#2344",
+            orderid: uuidv4(),
+        });
+    };
+    const handleOnClick1 = () => {
+        axios.put(process.env.REACT_APP_API_URL + "/v1.1/orderlists/" + 4, {
+            title: "Beyond Set",
+            status: "Waiting for review",
+            discord: "Nice#2344",
+            orderid: uuidv4(),
+            comment: "hello",
+        });
+    };
+    const handleOnClick2 = () => {
+        axios.delete(process.env.REACT_APP_API_URL + "/v1.1/orderlists/" + 4);
+    };
     React.useEffect(() => {
         axios({
-            method: "GET",
-            url: "http://localhost:3000/api/v1/orderlists",
+            method: "get",
+            url: process.env.REACT_APP_API_URL + "/v1.1/orderlists/",
         }).then((res) => {
             console.log(res.data);
+            console.log(uuidv4());
             setOrders(res.data);
         });
     }, []);
@@ -21,6 +42,12 @@ const Orders = () => {
             ))}
             <button className="" onClick={handleOnClick}>
                 Submit Order
+            </button>
+            <button className="" onClick={handleOnClick1}>
+                Update Order
+            </button>
+            <button className="" onClick={handleOnClick2}>
+                Delete Order
             </button>
         </div>
     );
