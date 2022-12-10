d134642b433983904e4e7b979fcb4bb9736ff570
search <search35453@gmail.com>
2022-12-10 12:36:42 +0700

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Commissions from "./pages/Commissions";
 import Contacts from "./pages/Contacts";
 import Orders from "./pages/Orders";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Contacts />} />
-			<Route path="/orders" element={<Orders />} />
-		</Routes>
-	);
+    return (
+        <Routes>
+            <Route path="/" element={<Contacts />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/commissions" element={<Commissions />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/pages/Commissions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Commissions = () => {
+    return <div>Commissions</div>;
+};
+
+export default Commissions;
