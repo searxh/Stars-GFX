c78fc2e6d0d7dee3b7399b84c9d577e64fd0760b
search <search35453@gmail.com>
2022-12-25 01:01:01 +0700

    add portfolio json

---
+++ src/lib/portfolio.ts
@@ -0,0 +1,305 @@
+const gfx = [
+    {
+        name: "Unromantic Love-A Cool Season.",
+        src: "https://media.discordapp.net/attachments/274862970587316224/1055835796931887156/Unromantic_Love.jpg?width=1830&height=1029",
+    },
+    {
+        name: "The Way of the Superior Man",
+        src: "https://media.discordapp.net/attachments/274862970587316224/1055836012699451392/The_Way_of_the_Superior_Man.jpeg?width=1830&height=1029",
+    },
+    {
+        name: "Modernization",
+        src: "https://media.discordapp.net/attachments/404632159866060810/1046299158195208312/Modernize.jpg?width=1830&height=1029",
+    },
+    {
+        name: "Little Natalie's Dream",
+        src: "https://media.discordapp.net/attachments/404632159866060810/1040996961509183488/Little_Natalies_Dream_Low_Res.png?width=1830&height=1029",
+    },
+    {
+        name: "Into a New Journey",
+        src: "https://media.discordapp.net/attachments/404632159866060810/881186767896727572/Into-A-New-Journeylowres.png?width=1716&height=1030",
+    },
+    {
+        name: "Dead Meat",
+        src: "https://media.discordapp.net/attachments/404632159866060810/878647328221855805/Dead-Meat-Low-Res.png?width=1716&height=1030",
+    },
+    {
+        name: "See You In Next Life",
+        src: "https://media.discordapp.net/attachments/404632159866060810/876110531579236382/See-you-Next-Life-Low-Res.png?width=1716&height=1030",
+    },
+    {
+        name: "Stepping into Metaverse",
+        src: "https://media.discordapp.net/attachments/404632159866060810/858360239044165652/Stepping-into-the-Metaverse.png?width=1830&height=1029",
+    },
+    {
+        name: "Hey Little Boy",
+        src: "https://media.discordapp.net/attachments/404632159866060810/855816721179869254/HeyLittleBoy.png?width=1830&height=1029",
+    },
+    {
+        name: "Like Father Like Son",
+        src: "https://media.discordapp.net/attachments/404632159866060810/853280055470718976/Like-father-like-son.png?width=1830&height=1029",
+    },
+    {
+        name: "ROBLOX VIRTUAL ASSISTANT",
+        src: "https://media.discordapp.net/attachments/404632159866060810/850023041282998292/Roblox-Virtual-assistant.png?width=1830&height=1029",
+    },
+    {
+        name: "Pipe Dream",
+        src: "https://media.discordapp.net/attachments/404632159866060810/845669965516242964/PipeDream.png?width=1830&height=1029",
+    },
+    {
+        name: "See you again",
+        src: "https://media.discordapp.net/attachments/404632159866060810/842770888177090610/See-you-again.png?width=1830&height=1029",
+    },
+    {
+        name: "Ding Dong",
+        src: "https://media.discordapp.net/attachments/404632159866060810/840236421011865600/DingDong.png?width=1830&height=1029",
+    },
+
+    {
+        name: "SCAMMERS!",
+        src: "https://media.discordapp.net/attachments/404632159866060810/832986579051347978/Keep_your_Account_Safe.png?width=1830&height=1029",
+    },
+    {
+        name: "SIMP",
+        src: "https://media.discordapp.net/attachments/404632159866060810/827913121250148352/SIMP.png?width=1830&height=1029",
+    },
+    {
+        name: "Dora The Explorer",
+        src: "https://media.discordapp.net/attachments/404632159866060810/825013756496052259/Dora-The-Explorer.png?width=1830&height=1029",
+    },
+    {
+        name: "Perfecto",
+        src: "https://media.discordapp.net/attachments/404632159866060810/815591683689283604/PERFECTO.png?width=1830&height=1029",
+    },
+    {
+        name: "Smart Plan",
+        src: "https://media.discordapp.net/attachments/404632159866060810/813055610668843029/Smart-Plan.png?width=1830&height=1029",
+    },
+    {
+        name: "Unexpired",
+        src: "https://media.discordapp.net/attachments/404632159866060810/810518481510072340/unexpired.png?width=1830&height=1029",
+    },
+    {
+        name: "Ready for some hunting?",
+        src: "https://media.discordapp.net/attachments/404632159866060810/807619332410572840/Ready4SOMEHUNTING.png?width=1830&height=1029",
+    },
+    {
+        name: "We're not a pop band!",
+        src: "https://media.discordapp.net/attachments/404632159866060810/805082463151063050/WERE_NOT_A_POP_BAND.png?width=1830&height=1029",
+    },
+    {
+        name: "Azurewrath's Hideout",
+        src: "https://media.discordapp.net/attachments/404632159866060810/802546137180798976/Azurewraths-hideout.png?width=1830&height=1029",
+    },
+    {
+        name: "Fellow Crew",
+        src: "https://media.discordapp.net/attachments/404632159866060810/800009012086439966/Fellow-Crew.png?width=1830&height=1029",
+    },
+    {
+        name: "My Lady",
+        src: "https://media.discordapp.net/attachments/404632159866060810/797834710151331916/MYLADY.png?width=1830&height=1029",
+    },
+    {
+        name: "Fred n George",
+        src: "https://media.discordapp.net/attachments/404632159866060810/794212006185402368/FrednGeorgefinal.png?width=1830&height=1029",
+    },
+    {
+        name: "Holy Present",
+        src: "https://media.discordapp.net/attachments/404632159866060810/792036451915005953/HOLY-PRESENT.png?width=1830&height=1029",
+    },
+    {
+        name: "We're on our way!",
+        src: "https://media.discordapp.net/attachments/404632159866060810/790224599156523028/WE-ARE-ON-OUR-WAY.png?width=1830&height=1029",
+    },
+    {
+        name: "Mission Complete We'll get them next time",
+        src: "https://media.discordapp.net/attachments/404632159866060810/787326423269376000/image0.png?width=1830&height=1029",
+    },
+    {
+        name: "Into the unknown",
+        src: "https://media.discordapp.net/attachments/404632159866060810/784789049155780658/into-the-unknown.png?width=1830&height=1029",
+    },
+    {
+        name: "Good old days",
+        src: "https://media.discordapp.net/attachments/404632159866060810/779715271614529556/GoodOldDays.png?width=1830&height=1029",
+    },
+    {
+        name: "OffAutumn",
+        src: "https://media.discordapp.net/attachments/404632159866060810/777182195542261800/OffAutumn.png?width=1830&height=1029",
+    },
+    {
+        name: "Content Deleted",
+        src: "https://cdn.discordapp.com/attachments/404632159866060810/774641829059756042/-Content-Deleted-.png",
+    },
+    {
+        name: "Happy Halloween",
+        src: "https://cdn.discordapp.com/attachments/404632159866060810/772105167063613440/HauntedHouseAmusementPark.png",
+    },
+    {
+        name: "Prank Gone Wrong",
+        src: "https://cdn.discordapp.com/attachments/404632159866060810/768410600623177728/Prank-Gone-Wrong.png",
+    },
+    {
+        name: "Endless Love",
+        src: "https://cdn.discordapp.com/attachments/404632159866060810/765959279529295912/Endless-Love.png",
+    },
+    {
+        name: "Under The Deep Ocean",
+        src: "https://cdn.discordapp.com/attachments/404632159866060810/763718115396943902/Under-the-deep-ocean.png",
+    },
+];
+
+const art = [
+    {
+        name: "Listen to MAMA",
+        desc: "On a Sunny day on my way back home. I found myself listening to an old r&b soul song, and ran into the song titled mama said by the shirelles. Right away I saw a bicycle, it reminds me of my young day when me and my bro were chasing each other on a bicycle haha.\nWASSUP, Listen to MAMA, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/1011908073046163516/Listen2Mama.png?width=728&height=1030",
+    },
+    {
+        name: "Daily Confusion of Youth",
+        desc: "Wondering what the meaning is? It’s nothing but you can fill it in over time tho x\nWASSUP, Daily Confusion of Youth, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/975021593245908992/Daily-Confusion-of-Youth.png?width=728&height=1030",
+    },
+    {
+        name: "Thanks for making my mundane egg not so mundane",
+        desc: "Your daily morning eggs 🥚 🍳\nWASSUP, Thanks for making my mundane egg not so mundane, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/970624297817214986/Thanks_for_making_my_mundane_egg_not_so_mundane.png?width=728&height=1030",
+    },
+    {
+        name: "You Got Fooled",
+        desc: "Happy Fools' DayHave a great month!\nWASSUP, You Got Fooled, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/959356537426554890/You-Got-Fooled.png?width=728&height=1030",
+    },
+    {
+        name: "Not A Failure",
+        desc: "Experimenting with DIY silkscreen print x\nWassup, Not A Failure, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/957285788700254248/Not-a-Failure.png?width=728&height=1030",
+    },
+    {
+        name: "+ to ×",
+        desc: "WASSUP, + to ×, 2022\nLearning how to steal and hide",
+        src: "https://media.discordapp.net/attachments/298413826280521728/956852457017864202/-to-x.png?width=728&height=1030",
+    },
+    {
+        name: "Blinded by advertisement",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/956495680451526656/Blinded-By-Advertisement.png?width=728&height=1030",
+    },
+    {
+        name: "Sususasa",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/955447995736944690/SUSUSASA.png?width=728&height=1030",
+    },
+    {
+        name: "Bestie",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/950702242586054656/Bestie.png?width=728&height=1030",
+    },
+    {
+        name: "Confident Boost",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/950353862739492904/Confident-Boost.png?width=728&height=1030",
+    },
+    {
+        name: "Political Tension",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/945541411107901470/Productive-Tension-of-Creativity.png?width=728&height=1030",
+    },
+    {
+        name: "Productive Tension of Creativity",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/946401501968994314/Political-Tension.png?width=728&height=1030",
+    },
+    {
+        name: "Untitled",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/938364275867672637/Untitled.png?width=1030&height=1030",
+    },
+    {
+        name: "Scrambled Eggs",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/925295015545163786/Scrambled-eggs.png?width=1030&height=1030",
+    },
+    {
+        name: "Untitled",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/925295817026326538/egg-white.png?width=1030&height=1030",
+    },
+    {
+        name: "Bacon's friend",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/925294984448593950/Bacons-Friend.png?width=1030&height=1030",
+    },
+    {
+        name: "13th DEC and nothing happened",
+        desc: "",
+        src: "https://media.discordapp.net/attachments/298413826280521728/913725831556247552/13-Dec.png?width=1030&height=1030",
+    },
+];
+
+const projects = [
+    {
+        name: "Inner Soul",
+        src: "https://media.discordapp.net/attachments/274862970587316224/1055839290447368223/Thumbnail5.png?width=1830&height=1029",
+    },
+    {
+        name: "Internet Scary World",
+        src: "https://media.discordapp.net/attachments/274862970587316224/1055839621713514566/INTERNET-SCARY-WORLD-V4.png?width=1830&height=1029",
+    },
+];
+
+const photos = [
+    {
+        src: "https://media.discordapp.net/attachments/274862970587316224/1055840734659485837/seen-1dfHq_ObJ4s-unsplash.jpg?width=1374&height=1031",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840735187972196/seen-bV2GJ-87jZk-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840742771265666/seen-D7t71_Ro0U4-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840750509760522/seen-Dhoy-bWjv-I-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840755404521583/seen-FKuMfKTEXNc-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840762044104755/seen-iME1ZgiM0EA-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840768037761065/seen-k4x6ZKMRtoc-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840779102339132/seen-k5ii_6DR-YA-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840781967040512/seen-KHHoOhnWrjc-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840795170709545/seen-m4ZDLOY5QYc-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840801432809562/seen-NArUoLRPfrI-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840802108100828/seen-nG7msQZyoBY-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840810580582430/seen-QQgABxtPJqc-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840811331366942/seen-STdnpmQfrNo-unsplash.jpg",
+    },
+    {
+        src: "https://cdn.discordapp.com/attachments/274862970587316224/1055840818763677706/seen-XdRG7R-DKUE-unsplash.jpg",
+    },
+];
+
+export const portfolio = {
+    gfx: gfx,
+    art: art,
+    projects: projects,
+    photos: photos,
+};
