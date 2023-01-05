da7dbb535d0de9e773f293719cc70ef11dc4d8e0
search <search35453@gmail.com>
2023-01-05 20:17:07 +0700

    update about page style + responsive design

--- src/components/about/ReviewItem.tsx
+++ src/components/about/ReviewItem.tsx
@@ -4,8 +4,14 @@ import { ReviewItemType } from "../../types";
 const ReviewItem = ({ reviewItem }: { reviewItem: ReviewItemType }) => {
     const { content, date } = reviewItem;
     return (
-        <div className="w-full bg-white rounded-xl p-5">
-            <div>{content}</div> <div>{date}</div>
+        <div
+            className="relative flex h-full w-[22rem] md:w-[35rem] my-2 mx-auto bg-white 
+            p-5 shadow-md bg-opacity-10 text-white drop-shadow-md border-2 rounded-3xl"
+        >
+            <div className="md:m-auto mx-auto pb-5">
+                <div className="text-lg font-semibold">"{content}"</div>
+            </div>
+            <div className="absolute left-0 right-0 bottom-3">{date}</div>
         </div>
     );
 };
--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -1,62 +1,8 @@
 export const reviews = [
     {
-        content: "Amazing 10/10 GFX",
-        date: "30/04/2020",
-    },
-    {
-        content: "Fast, amazing, and gets the gfx done on time 10/10",
-        date: "03/06/2020",
-    },
-    {
         content:
-            "The best quality and quickest GFX artist on the market 9001/9001",
-        date: "04/06/2020",
-    },
-    {
-        content:
-            "Fast & Quality GFX if you ever need any made in a short period of time (mine was made in roughly 2 hours)",
-        date: "04/07/2020",
-    },
-    {
-        content:
-            "Amazing gfx made in only 3 hours. Great customer service too which is rare to come by. Don’t sleep on this man",
-        date: "23/08/2020",
-    },
-    {
-        content:
-            "Star is an amazing gfx artist and overall person. Hes an actual joy to work with and he thinks so far beyond to really give you the best work that he can possibly make. I 100% recommended ordering from him.",
-        date: "09/10/2020",
-    },
-    {
-        content:
-            "Star is a great gfx artist, got my second game icon from him really is super good and way better than i expected, 100 percent worth the money go to him when you need a render he is my go to guy as of now!",
-        date: "19/11/2020",
-    },
-    {
-        content:
-            "I’m shocked, because like Star did my work in like 5 hours and in one day… this is my first 2nd GFX icon and wow… this one has topped anything I’ve seen from any artists out there. I definitely recommend ordering!",
-        date: "01/01/2021",
-    },
-    {
-        content:
-            "Extremely Talented. he incorporated absolute realism into the thumbnail and made it exactly how I pictured it to be. Highly recommend commissioning! Love it.",
-        date: "09/01/2021",
-    },
-    {
-        content:
-            "Star is so incredibly talented, and really took the thumbnail to another level. I didn't have the best idea of what to have for a background on it, and he made the project his own and did it better than I could have imagined. He also worked extremely quickly, and got it to me within a few days. I 100% recommend him.",
-        date: "23/03/2021",
-    },
-
-    {
-        content:
-            "Absolutely beyond expectations! Somehow put everything I pictured and more in to the Thumbnail I got! I would recommend him to anyone! Thank you again and please give him the chance he'll blow your mind away with his amazing creations! Also super fast delivery, didn't take much time at all, cheap price, I mean you literally can't beat that!!! Great work Star and thank you again!!!",
-        date: "29/03/2021",
-    },
-    {
-        content:
-            "Great quality and amazing workflow. I would definitely recommend hiring star. You'll always get your desired product on time.",
-        date: "28/10/2022",
+            "Amazing as always, loved working w him and can't wait until the next time!",
+        date: "21/11/2022",
     },
     {
         content:
@@ -65,7 +11,60 @@ export const reviews = [
     },
     {
         content:
-            "Amazing as always, loved working w him and can't wait until the next time!",
-        date: "21/11/2022",
+            "Great quality and amazing workflow. I would definitely recommend hiring star. You'll always get your desired product on time.",
+        date: "28/10/2022",
+    },
+    {
+        content:
+            "Absolutely beyond expectations! Somehow put everything I pictured and more in to the Thumbnail I got! I would recommend him to anyone! Thank you again and please give him the chance he'll blow your mind away with his amazing creations! Also super fast delivery, didn't take much time at all, cheap price, I mean you literally can't beat that!!! Great work Star and thank you again!!!",
+        date: "29/03/2021",
+    },
+    {
+        content:
+            "Star is so incredibly talented, and really took the thumbnail to another level. I didn't have the best idea of what to have for a background on it, and he made the project his own and did it better than I could have imagined. He also worked extremely quickly, and got it to me within a few days. I 100% recommend him.",
+        date: "23/03/2021",
+    },
+    {
+        content:
+            "Extremely Talented. he incorporated absolute realism into the thumbnail and made it exactly how I pictured it to be. Highly recommend commissioning! Love it.",
+        date: "09/01/2021",
+    },
+    {
+        content:
+            "I’m shocked, because like Star did my work in like 5 hours and in one day… this is my first 2nd GFX icon and wow… this one has topped anything I’ve seen from any artists out there. I definitely recommend ordering!",
+        date: "01/01/2021",
+    },
+    {
+        content:
+            "Star is a great gfx artist, got my second game icon from him really is super good and way better than i expected, 100 percent worth the money go to him when you need a render he is my go to guy as of now!",
+        date: "19/11/2020",
+    },
+    {
+        content:
+            "Star is an amazing gfx artist and overall person. Hes an actual joy to work with and he thinks so far beyond to really give you the best work that he can possibly make. I 100% recommended ordering from him.",
+        date: "09/10/2020",
+    },
+    {
+        content:
+            "Amazing gfx made in only 3 hours. Great customer service too which is rare to come by. Don’t sleep on this man",
+        date: "23/08/2020",
+    },
+    {
+        content:
+            "Fast & Quality GFX if you ever need any made in a short period of time (mine was made in roughly 2 hours)",
+        date: "04/07/2020",
+    },
+    {
+        content:
+            "The best quality and quickest GFX artist on the market 9001/9001",
+        date: "04/06/2020",
+    },
+    {
+        content: "Fast, amazing, and gets the gfx done on time 10/10",
+        date: "03/06/2020",
+    },
+    {
+        content: "Amazing 10/10 GFX",
+        date: "30/04/2020",
     },
 ];
