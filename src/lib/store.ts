af28db902f5fe8c005cefd3106dae01715259b16
Nattakit Pisitsup <search35453@gmail.com>
2023-12-10 14:33:33 +0900

    Add and implement store page

---
+++ src/lib/store.ts
@@ -0,0 +1,27 @@
+export const store = [
+    {
+        name: "Light It Up",
+        link: "https://stargfx.gumroad.com/l/LightItUp",
+        price: 200,
+    },
+    {
+        name: "Star's Notion",
+        link: "https://stargfx.gumroad.com/l/bmavu",
+        price: 50,
+    },
+    {
+        name: "Glow Like a Star",
+        link: "https://stargfx.gumroad.com/l/yafclu",
+        price: 500,
+    },
+    {
+        name: "Roblox GFX Starter Pack",
+        link: "https://stargfx.gumroad.com/l/llnina",
+        price: 500,
+    },
+    {
+        name: "Roblox Wallpaper Pack",
+        link: "https://stargfx.gumroad.com/l/efboy",
+        price: 500,
+    },
+];
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import useScript from "../hooks/useScript";
+import { store } from "../lib/store";
+import StoreItem from "../components/store/StoreItem";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const Store = () => {
-    return <div>Store</div>;
+    const isSmallerThanMedium = useMediaQuery("(max-width: 786px)");
+    const isSmall = useMediaQuery("(max-width: 530px)");
+    useScript("https://gumroad.com/js/gumroad.js");
+    return (
+        <div
+            className="relative flex flex-col py-12 w-full min-h-screen h-full
+            font-nunito bg-neutral-100 text-center"
+        >
+            <div
+                className={`grid ${
+                    isSmall
+                        ? "grid-cols-1"
+                        : isSmallerThanMedium
+                        ? "grid-cols-2"
+                        : "grid-cols-3"
+                } gap-5 p-10 mx-auto`}
+            >
+                {store.map((item, index) => {
+                    const { name, price, link } = item;
+                    return (
+                        <StoreItem
+                            key={Math.random()}
+                            name={name}
+                            link={link}
+                            price={price}
+                            index={index}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Store;
