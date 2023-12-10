2cd87f6552a1b896f910631817da15e1bf75043d
Nattakit Pisitsup <search35453@gmail.com>
2023-12-10 14:32:33 +0900

    Switch navbar order

--- src/lib/default.ts
+++ src/lib/default.ts
@@ -2,10 +2,10 @@ import { GlobalStateType, PriceInfoType, StringToAnyType } from "../types";
 
 export const all_routes: StringToAnyType = {
     "": "About",
-    archives: "Archives",
     store: "Store",
     commissions: "Commissions",
     orders: "Your Orders",
+    archives: "Archives",
     contacts: "Contacts",
 };
 
