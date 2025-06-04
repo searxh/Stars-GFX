dc3f233bceff14f9ae057979fa5f0f42b77c1ca9
searxh <search35453@gmail.com>
2025-06-04 15:57:13 +0700

    Optimize bg video to webm

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -94,7 +94,7 @@ const About = () => {
                     }}
                     className="w-full h-full"
                 >
-                    <source src="/videos/about.mov" type="video/mp4"></source>
+                    <source src="/videos/about.webm" type="video/webm"></source>
                 </video>
                 <div className="absolute bottom-0 h-52 w-full bg-gradient-to-t from-slate-900 to-transparent" />
             </div>
