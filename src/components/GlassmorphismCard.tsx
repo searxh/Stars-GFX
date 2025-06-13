e3d1406076b1d9456c98dd8927bd22533999527c
searxh <search35453@gmail.com>
2025-06-13 17:31:55 +0700

    Add extra styles to glass morphism card

--- src/components/GlassmorphismCard.tsx
+++ src/components/GlassmorphismCard.tsx
@@ -2,8 +2,18 @@ import { ReactNode } from "react";
 
 export default function GlassmorphismCard({
     children,
+    className,
 }: {
     children: ReactNode;
+    className?: string;
 }) {
-    return <div className="h-fit w-fit p-[24px] bg-[#222222] rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-[100px] bg-opacity-[30%]">{children}</div>;
+    return (
+        <div
+            className={`h-fit w-fit ${
+                className ?? "p-6"
+            } bg-[#222222] rounded-3xl backdrop-blur-[100px] bg-opacity-[30%] border-[1px] border-neutral-800`}
+        >
+            {children}
+        </div>
+    );
 }
