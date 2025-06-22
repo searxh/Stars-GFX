ad813f985c625fb81bb8fcad125ede713371cfc2
searxh <search35453@gmail.com>
2025-06-22 17:51:33 +0700

    Add style in glassmorphism card

--- src/components/GlassmorphismCard.tsx
+++ src/components/GlassmorphismCard.tsx
@@ -3,15 +3,19 @@ import { ReactNode } from "react";
 export default function GlassmorphismCard({
     children,
     className,
+    style,
 }: {
     children: ReactNode;
     className?: string;
+    style?: React.CSSProperties;
 }) {
     return (
         <div
-            className={`h-fit w-fit ${
-                className ?? "p-6"
-            } bg-[#222222] rounded-3xl backdrop-blur-[100px] bg-opacity-[30%] border-[1px] border-neutral-800`}
+            style={style}
+            className={`${
+                className ?? "p-6 h-fit w-fit"
+            } bg-[#222222] rounded-3xl backdrop-blur-[100px] 
+            backdrop-filter bg-clip-padding bg-opacity-[40%] border-[1px] border-neutral-800`}
         >
             {children}
         </div>
