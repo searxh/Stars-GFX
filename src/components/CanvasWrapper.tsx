97dce02a6e90b44e8074b35521961c8a2cbb6a68
searxh <search35453@gmail.com>
2025-06-30 12:45:28 +0700

    Add react three render components

---
+++ src/components/CanvasWrapper.tsx
@@ -0,0 +1,36 @@
+import { CircularProgress } from "@mui/material";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+
+interface CanvasWrapperProps {
+    children: React.ReactNode;
+    className?: string;
+    defaultFallbackSize?: { width: number; height: number };
+}
+
+const CanvasWrapper = ({
+    children,
+    className,
+    defaultFallbackSize = { width: 120, height: 120 },
+}: CanvasWrapperProps) => {
+    const { width, height } = defaultFallbackSize;
+    return (
+        <Suspense
+            fallback={
+                <div style={{ width, height }} className="flex m-auto">
+                    <CircularProgress color="error" className="m-auto" />
+                </div>
+            }
+        >
+            <Canvas
+                style={{ height: "100%", width: "100%" }}
+                shadows={true}
+                className={className}
+            >
+                {children}
+            </Canvas>
+        </Suspense>
+    );
+};
+
+export default CanvasWrapper;
