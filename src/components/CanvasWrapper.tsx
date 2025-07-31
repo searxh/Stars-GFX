60a2a077fd04e7859d73977b50be01e3abd380ea
searxh <search35453@gmail.com>
2025-07-31 14:07:05 +0700

    Add fancier loading

--- src/components/CanvasWrapper.tsx
+++ src/components/CanvasWrapper.tsx
@@ -1,30 +1,27 @@
-import { CircularProgress } from "@mui/material";
+import { Box, CircularProgress, LinearProgress } from "@mui/material";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
 
 interface CanvasWrapperProps {
     children: React.ReactNode;
     className?: string;
-    defaultFallbackSize?: { width: number; height: number };
 }
 
-const CanvasWrapper = ({
-    children,
-    className,
-    defaultFallbackSize = { width: 120, height: 120 },
-}: CanvasWrapperProps) => {
-    const { width, height } = defaultFallbackSize;
+const LoaderOverlay = () =>
+    ReactDOM.createPortal(
+        <div className="fixed top-0 flex h-screen w-screen bg-black text-white z-10 items-center justify-center">
+            <div className="flex flex-col gap-2 w-36 m-auto">
+                <div className="mx-auto text-sm">LOADING...</div>
+                <LinearProgress color="inherit" />
+            </div>
+        </div>,
+        document.body
+    );
+
+const CanvasWrapper = ({ children, className }: CanvasWrapperProps) => {
     return (
-        <Suspense
-            fallback={
-                <div
-                    style={{ width, height }}
-                    className="flex m-auto h-full w-full"
-                >
-                    <CircularProgress color="inherit" className="m-auto" />
-                </div>
-            }
-        >
+        <Suspense fallback={<LoaderOverlay />}>
             <Canvas
                 style={{ height: "100%", width: "100%" }}
                 shadows={true}
